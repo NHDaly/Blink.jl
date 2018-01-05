5ed3c95e8ff8af8824385645c1d61564ce482063
Nathan Daly <nhdaly@gmail.com>
2018-01-04 20:31:45 -0600

    tentatively trying to update the menu.

--- src/AtomShell/main.js
+++ src/AtomShell/main.js
@@ -1,6 +1,5 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow} = require('electron')
 var net = require("net");
-var Menu = require("menu");
 
 // console.log('Args:');
 // console.log(process.argv);
@@ -86,7 +85,7 @@ function createWindow(opts) {
     delete windows[win_id];
   });
 
-  console.log("requiring Menu");
+  setupApplicationMenu();
 
   return win.id;
 }
@@ -103,32 +102,97 @@ function withwin(id, code) {
   }
 }
 
-// -----------------------------------
-// -- Trying to set up a Menu per Issue #101.
-// -----------------------------------
-// THIS FAILS with `Error: Cannot find module 'menu'`
-//console.log("requiring Menu");
-//var Menu = require("menu");
-//console.log("Menu: ", Menu);
-//
-//var template = [{
-//    label: "Application",
-//    submenu: [
-//        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-//        { type: "separator" },
-//        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-//    ]}, {
-//    label: "Edit",
-//    submenu: [
-//        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-//        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-//        { type: "separator" },
-//        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-//        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-//        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-//        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-//    ]}
-//];
-//
-//Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-//console.log("Finished setting Application Menu");
+function setupApplicationMenu() {
+
+  const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://electronjs.org') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  template[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  template[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
+console.log("Finished setting Application Menu");
+
+}
